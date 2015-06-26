0ee761e3f29783f37f5cf845ad124b1a64aa95af
Ruben de Gooijer <rdegooijer@xebia.com>
2015-06-26 12:01:01 +0200

    second

--- src/main/java/App.scala
+++ src/main/java/App.scala
@@ -3,35 +3,89 @@ import akka.typed.ScalaDSL._
 import akka.typed.AskPattern._
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.concurrent.Await
 import akka.util._
+import scala.util.Random
 
-object HelloWorld {
-  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
-  final case class Greeted(whom: String)
- 
-  val greeter = Static[Greet] { msg ⇒
-    println(s"Hello ${msg.whom}!")
-    msg.replyTo ! Greeted(msg.whom)
-  }
+object Behaviours {
+
+	type SessionId = Int
+	sealed trait SessionCommand
+	case class OpenSession(replyTo: ActorRef[Session]) extends SessionCommand
+	case class CloseSession(id: SessionId) extends SessionCommand
+
+	case class Session(id: SessionId, basket: ActorRef[BasketRequest])
+
+	type Basket = List[BasketItem]
+	case class BasketItem(productName: String, price: Int, quantity: Int)
+
+	sealed trait BasketRequest
+	final case class Add(item: BasketItem) extends BasketRequest
+	final case class Remove(productName: String) extends BasketRequest
+	final case class Get(replyTo: ActorRef[Basket]) extends BasketRequest
+	case object Empty extends BasketRequest
+
+	sealed trait CheckoutRequest
+	case class Checkout(basket: Basket) extends CheckoutRequest
+
+	sealed trait CheckoutResponse
+	case class OutOfStock(basket: Basket) extends CheckoutResponse
+	case object Ok extends CheckoutResponse
+
+	type Stock = List[BasketItem]
+
+//	val closeSession: Behavior[CloseSession] = Static[CloseSession] { case CloseSession(id) => {
+//
+//	}}
+
+	def shopperBehaviour(sessions: Set[SessionId]): Behavior[SessionCommand] = ContextAware { ctx =>
+		Total {
+			case CloseSession(id) => {
+				shopperBehaviour(sessions - id)
+			}
+			case OpenSession(replyTo) => {
+				val sessionId = Random.nextInt()
+				val basketActor = ctx.spawn(Props(basketBehaviour(List.empty)), s"basket-${sessionId}")
+				val session = Session(sessionId, basketActor)
+				replyTo ! session
+				shopperBehaviour(sessions + session.id)
+			}
+		}
+	}
+
+	def basketBehaviour(basket: Basket): Behavior[BasketRequest] = Total {
+		case Add(item) => basketBehaviour(basket :+ item)
+		case Remove(productName) => basketBehaviour(basket.filterNot(_.productName == productName))
+		case Empty => basketBehaviour(List.empty)
+		case Get(replyTo) => replyTo ! basket ; Same
+	}
+
+	lazy val checkoutBehaviour = ???
 }
 
 
 object App {
-  def main(args: Array[String]) = {
-  	import HelloWorld._
+	import Behaviours._
 	// using global pool since we want to run tasks after system shutdown
 	import scala.concurrent.ExecutionContext.Implicits.global
-	
+
 	implicit val timeout = Timeout(3 seconds)
 
-	val system: ActorSystem[Greet] = ActorSystem("hello", Props(greeter))
-	
-	val future: Future[Greeted] = system ? (Greet("world", _))
-	 
-	for {
-	  greeting <- future.recover { case ex => ex.getMessage }
-	  done <- { println(s"result: $greeting"); system.terminate() }
-	} println("system terminated")
+	def main(args: Array[String]) = {
+		val system: ActorSystem[SessionCommand] = ActorSystem("shopper", Props(shopperBehaviour(Set.empty)))
+
+		val future = system ? OpenSession
+
+		for {
+			Session(id, basket) <- future.recover { case ex => ex.getMessage }
+			_ <- Future { println("id: ", id, "basket: ", basket) }
+
+			_ <- Future { basket ! Add(BasketItem("macbook", 1, 1000)) ; Thread.sleep(1000) }
+
+			items <- basket ? Get
+
+			_ <- Future { println("id: ", id, "basket: ", items) }
+
+			done <- { system.terminate() }
+		} println("system terminated")
   }
 }
